05ac2079c9fb2a1abacd7f526eba6cb3f9acb779
v0ltis <40314287+v0ltis@users.noreply.github.com>
2021-04-23 12:24:20 +0200

    Update levelup.py

--- FRank/cogs/levelup.py
+++ FRank/cogs/levelup.py
@@ -1,177 +1,35 @@
 import json
-import random
 
-import mysql.connector
+import discord
 
-import cogs.levelup
+import my.sql
 
 
-class Connexion:
-    def __init__(self):
-        self.db = mysql.connector.connect(user='frank', password='frank',
-                                          host='frank.frank',
-                                          database='frank')
+class Levelup:
+    @staticmethod
+    async def announce(bot, channel, user: int, level):
+        try:
+            channel = bot.get_channel(channel)
+            await channel.send(":tada: Bravo <@{0}>, tu es passé niveau {1} :tada: !".format(user, level))
+            g = channel.guild
+            data = await my.sql.Connexion().list_roles(g.id)
 
-        self.cursor = self.db.cursor(buffered=True)
+            if not (data == [] or len(json.loads(data[0][1])) == 0):
 
-    async def add_user(self, guild: int, uid: int):
-        req = "select * from users where guild = {} ".format(guild)
-        self.cursor.execute(req)
-        request = self.cursor.fetchall()
+                user = await g.fetch_member(user)
+                data = json.loads(data[0][1])
+                for x in data:
+                    if int(x) <= int(level):
+                        role = g.get_role(int(data[x]))
 
-        if not request:
-            insert = "insert into users VALUES ('{0}', '{{}}', null)".format(guild)
-            self.cursor.execute(insert)
-            self.db.commit()
-            data = {}
+                        if role is not None:
 
-        else:
-            data = json.loads(request[0][1])
+                            try:
+                                await user.add_roles(role)
 
-        data.update({uid: {"xp": 0, "level": 1}})
+                            except (discord.Forbidden, discord.HTTPException) as e:
+                                print(e)
+                                pass
 
-        update = "UPDATE users set data = '{0}' WHERE guild = {1}".format(json.dumps(data), guild)
-        self.cursor.execute(update)
-        self.db.commit()
-
-    async def add_xp(self, user: int, guild: int, bot, mchannel):
-        levels = await self.get_user(user, guild, only=False)
-
-        levels[str(user)]['xp'] += random.randint(2, 8)
-        level = levels[str(user)]['level']
-
-        count = round((((level ** 2) + 50 + (level * 10)) * 2.5))
-
-        if count <= levels[str(user)]['xp']:
-            levels[str(user)]['level'] += 1
-            channel = levels[0][2]
-
-            if channel is None:
-                channel = mchannel
-
-            await cogs.levelup.Levelup.announce(bot, channel, user, levels[str(user)]['level'])
-
-            levels[str(user)]['xp'] -= count
-
-        save = "UPDATE users set data = '{0}' where guild = {1}".format(json.dumps(levels), guild)
-
-        self.cursor.execute(save)
-
-        self.db.commit()
-
-    async def get_user(self, user, guild, only=True):
-        req = "SELECT * FROM users WHERE guild = '{}'".format(guild)
-
-        self.cursor.execute(req)
-        data = self.cursor.fetchall()
-
-        if data == [] or str(user) not in json.loads(data[0][1]).keys():
-            await self.add_user(guild, user)
-            self.cursor.execute(req)
-            data = self.cursor.fetchall()
-
-        json_data = json.loads(data[0][1])
-
-        if only:
-            return json_data[str(user)]
-
-        return json_data
-
-    async def get(self, guild):
-        req = "SELECT * FROM users WHERE guild = {}".format(guild)
-
-        self.cursor.execute(req)
-        return self.cursor.fetchall()[0]
-
-    async def setchannel(self, guild, channel):
-        if channel is not None:
-            req = "UPDATE users set channel = {0} WHERE guild = {1}".format(channel, guild)
-        else:
-            req = "UPDATE users set channel = NULL WHERE guild = {0}".format(guild)
-
-        self.cursor.execute(req)
-        self.db.commit()
-
-    async def update(self, guild, val, quant, user):
-        levels = await self.get_user(user, guild, only=False)
-        
-        levels[str(user)][val] += quant
-
-        save = "UPDATE users set data = '{0}' where guild = {1}".format(json.dumps(levels), guild)
-
-        self.cursor.execute(save)
-        self.db.commit()
-
-    async def delete(self, user: int, guild: int):
-        req = "SELECT * FROM users WHERE guild = '{}'".format(guild)
-
-        self.cursor.execute(req)
-
-        data = json.loads(self.cursor.fetchall()[0][1])
-
-        if not str(user) in data.keys():
-            return False
-
-        del data[str(user)]
-
-        save = "UPDATE users set data = '{0}' where guild = {1}".format(json.dumps(data), guild)
-
-        self.cursor.execute(save)
-        self.db.commit()
-        return True
-
-    async def add_role(self, role, guild, level):
-
-        req = "SELECT * FROM roles WHERE guild = '{}'".format(guild)
-
-        self.cursor.execute(req)
-
-        data = self.cursor.fetchall()
-
-        if not data:
-            act = "insert into roles VALUES ('{0}', '{{}}')".format(guild)
-            self.cursor.execute(act)
-            self.db.commit()
-            self.cursor.execute(req)
-            data = self.cursor.fetchall()
-
-        roles = json.loads(data[0][1])
-
-        roles[str(level)] = str(role)
-
-        save = "UPDATE roles set data = '{0}' where guild = {1}".format(json.dumps(roles), guild)
-        self.cursor.execute(save)
-        self.db.commit()
-
-    async def del_role(self, guild, role):
-        req = "SELECT * FROM roles WHERE guild = {}".format(guild)
-
-        self.cursor.execute(req)
-        data = self.cursor.fetchall()
-
-        if data == [] or str(role) not in json.loads(data[0][1]).values():
-            return False
-
-        data = json.loads(data[0][1])
-        for key in list(data):
-            if str(role) == str(data[key]):
-                del data[key]
-
-        save = "UPDATE roles set data = '{0}' where guild = {1}".format(json.dumps(data), guild)
-
-        self.cursor.execute(save)
-        self.db.commit()
-        return True
-
-    async def list_roles(self, guild):
-        req = "SELECT * FROM roles WHERE guild = {}".format(guild)
-
-        self.cursor.execute(req)
-        return self.cursor.fetchall()
-
-    async def reset(self, guild):
-        req = "UPDATE users set data = '{{}}' where guild = {0}".format(guild)
-
-        self.cursor.execute(req)
-
-        self.db.commit()
+        except discord.Forbidden:
+            pass
